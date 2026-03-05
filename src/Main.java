34aa3fb95bee2cf0ed45bb0b3756ad33e7c7de98
Bozok <bozokokan11@hotmail.com>
2026-03-05 20:57:04 +0100

    Copy file

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String... args)  {
+        File copyFile = new File("copyfile.txt");
+
+        try{
+            FileReader copyFileReader = new FileReader(copyFile);
+            System.out.println(copyFileReader.readAllAsString());
+            System.out.println("Dali zelite kopirati datoteku copyfile.txt?");
+            Scanner input = new Scanner(System.in);
+            String odg = input.nextLine();
+            if(odg.contentEquals("da")){
+                String desktopPath = System.getProperty("user.home") + File.separator + "Desktop" + File.separator + "copyFile.txt";
+                FileInputStream in = new FileInputStream(copyFile);
+                FileOutputStream out = new FileOutputStream(desktopPath);
+
+
+                int c;
+                while((c = in.read()) != -1){
+                    out.write(c);
+                }
+                in.close();
+                out.close();
+                System.out.println("Datoteka kopirana na lokaciji: " + desktopPath);
+            }
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("File not found!");
+        } catch (IOException e) {
+            throw new RuntimeException("Greska: No data!");
+        }
+    }
+}
